43c6399c64f3a5102e51149d32ca958980ace607
MohaDarkness <Mohannad.Atmeh@gmail.com>
2022-01-24 01:21:30 +0200

    Final commit

--- myscript.js
+++ myscript.js
@@ -1,20 +1,5 @@
 var p, q, e, n, phi, d;
 
-// var x, y;
-// x = "96";
-// y = "100";
-// // x = [];
-// // x.push("123123");
-// console.log(x>y);
-// for(var i = 1; i < 127; i++){
-//     if(i < 32)
-//         var x = String.fromCharCode(i+32);
-//     else
-//         var x = String.fromCharCode(i);
-//     console.log(i, x);
-// }
-
-
 function check1() {
     p = document.getElementById("pValue").value;
     console.log(p);
@@ -24,6 +9,9 @@ function check1() {
             console.log(p);
             document.getElementById("qValue").removeAttribute("readonly");
             console.log("now you can use the second!");
+        }
+        else{
+            alert("Not a prime number!!\nPlease enter another number and it must be prime.");
         }
     }
     else {
@@ -42,6 +30,9 @@ function check2() {
             calculateValues();
             document.getElementById("eValue").removeAttribute("readonly");
             console.log("now you can use the third!");
+        }
+        else{
+            alert("Not a prime number!!\nPlease enter another number and it must be prime.");
         }
     }
     else {
@@ -69,12 +60,13 @@ function calculateValues() {
 function check3() {
     e = document.getElementById("eValue").value;
     e = math.bignumber(e);
-    if (math.gcd(e, phi) == 1) {
+    if (math.gcd(e, phi) == 1 && e*1 > 1 && e*1 < phi) {
         console.log(e);
         document.getElementById("plaintext").removeAttribute("readonly");
         calculateD(e, phi);
     }
     else {
+        alert("Invalid value of e!!\nPlease enter another number and make sure it is a coprime with phi="+phi+" and it must be 1 < e < phi.");
         console.log("Wrong E!!");
     }
 }
@@ -90,58 +82,22 @@ function calculateD(E, Phi) {
         }
     }
     console.log("This is d: ", d * 1);
-    // console.log("This is egcd: ", a["value"]*1, b["value"]*1, c["value"]*1);
-    // var x0 = 0, x1 = 1, y0 = 1, y1 = 0, q, tmpE;
-    // while (E != 0) {
-    //     q = parseInt(Phi / E);
-    //     console.log("This is Phi, E, q : ", Phi, E, q);
-    //     tmpE = E;
-    //     E = Phi % E;
-    //     Phi = tmpE;
-    //     // [q, E, Phi] = [parseInt(Phi/E), Phi%E, E]
-    //     console.log("This is y0, y1 before: ", y0, y1);
-    //     [y0, y1] = [y1, y0 - q * y1]
-    //     console.log("This is y0, y1 After: ", y0, y1);
-    //     console.log("This is x0, x1 before: ", x0, x1);
-    //     [x0, x1] = [x1, x0 - q * x1]
-    //     console.log("This is x0, x1 After: ", x0, x1);
-    // }
-    // if (x0 > 0) {
-    //     d = x0;
-    // }
-    // else {
-    //     x0 = BigInt(x0);
-    //     while (x0 <= 0) {
-    //         x0 += Phi;
-    //     }
-    //     d = x0;
-    // }
-    // console.log("This is d: ",d);
-    prints();
 }
 
-function prints() {
-    console.log("This is p:", p);
-    console.log("This is q:", q);
-    console.log("This is n:", n);
-    console.log("This is phi:", phi);
-    console.log("This is d:", d);
-}
-
-var Plain, plainSplit, Cipher, cipherSplit, m, c;
+var Plain, plainSplit, Cipher, cipherSplit, plainAgain, plainAgainSplit;
 
 function takePlain() {
     Plain = document.getElementById("plaintext").value;
-    console.log(Plain);
+    console.log("This is the plaintext:",Plain);
     plainSplit = Plain.split("");
     for (let i = 0; i < plainSplit.length; i++) {
         plainSplit[i] = '' + plainSplit[i].charCodeAt(0);
-        if ('' + plainSplit[i] < 100) {
-            console.log("are we here?");
+        if ('' + plainSplit[i] < 100) 
             plainSplit[i] = "0" + plainSplit[i];
-        }
+        
     }
     console.log("Plaintext converted to ascii array: ", plainSplit);
+    
     protocol();
 }
 var protocolNum;
@@ -177,22 +133,53 @@ function prepare() {
         }
         plainSplit.push(st);
     }
+    else{
+        plainSplit.push(st);
+    }
     console.log("Plaintext converted to ascii blocks: ", plainSplit);
 
     lowLevelEncryption();
 }
 
+
+function powerMod(x, y, p)
+{
+    // Initialize result
+    let res = 1;
+ 
+    // Update x if it is more
+    // than or equal to p
+    x = x % p;
+ 
+    if (x == 0)
+        return 0;
+ 
+    while (y > 0)
+    {
+        // If y is odd, multiply
+        // x with result
+        if (y & 1)
+            res = (res * x) % p;
+ 
+        // y must be even now
+         
+        // y = $y/2
+        y = y >> 1;
+        x = (x * x) % p;
+    }
+    return res;
+}
+
 function lowLevelEncryption() {
     var tmpNum;
     cipherSplit = [];
-    console.log("This is n, n*1:", n, n * 1);
+    // console.log("This is n, n*1:", n, n * 1);
     for (var i = 0; i < plainSplit.length; i++) {
-        tmpNum = math.bignumber(plainSplit[i])
-        tmpNum = math.pow(tmpNum, e);
-        tmpNum = math.mod(tmpNum, n);
+        tmpNum = math.bignumber(plainSplit[i]);
+        tmpNum = powerMod(tmpNum, e, n);
         cipherSplit.push('' + tmpNum);
     }
-    console.log("This is cipherSplit: ", cipherSplit);
+    // console.log("This is cipherSplit: ", cipherSplit);
 
     highLevelEncryption();
 }
@@ -222,6 +209,66 @@ function highLevelEncryption() {
             tmpConv += 32;
         Cipher += String.fromCharCode(tmpConv);
     }
-    console.log(Cipher);
+    console.log("This is the ciphertext:",Cipher);
+    lowLevelDecrypt();
 }
 
+
+
+function lowLevelDecrypt(){
+    plainAgainSplit = [];
+    tmpCipherSplit = cipherSplit;
+    var tmp = 0;
+    console.log(cipherSplit);
+    for(var i = 0; i < tmpCipherSplit.length; i++){
+        tmp = math.bignumber(tmpCipherSplit[i]);
+        // console.log("This is tmp before pow:",tmp*1);
+        // tmp = math.pow(tmp, d);
+        // console.log("This is tmp before mod:",tmp*1);
+        // tmp = math.mod(tmp, n);
+        // console.log("This is tmp before push:",tmp*1);
+        // plainAgainSplit.push(''+tmp);
+        tmp = powerMod(tmp, d, n);
+        console.log("This is tmp:",tmp);
+        plainAgainSplit.push(''+tmp);
+    }
+    for(var i = 0; i < plainAgainSplit.length; i++){
+        var st = '';
+        for(var j = 0; j < protocolNum*3 - plainAgainSplit[i].length; j++)
+            st += '0';
+        
+        plainAgainSplit[i] = st + plainAgainSplit[i];
+    }
+    highLevelDecryption()
+}
+
+function highLevelDecryption(){
+    if(protocolNum != 1){
+        var tmpSplit = plainAgainSplit;
+        plainAgainSplit = []
+        for(var i = 0; i < tmpSplit.length; i++){
+            var newNum = tmpSplit[i][0];
+            for(j = 1; j < protocolNum*3; j++){
+                if(j % 3 == 0){
+                    plainAgainSplit.push(newNum);
+                    newNum = tmpSplit[i][j];
+                }   
+                else{
+                    newNum += tmpSplit[i][j];
+                }
+            }
+            plainAgainSplit.push(newNum);
+        }
+    }
+
+    // console.log(plainAgainSplit);
+    plainAgain = '';
+    for(var i = 0; i < plainAgainSplit.length; i++){
+        plainAgain += String.fromCharCode(parseInt(plainAgainSplit[i]));
+    }
+    console.log("This is plainSplit:",plainSplit)
+    console.log("This is cipherSplit:",cipherSplit)
+    console.log("This is plainAgainSplit:",plainAgainSplit);
+    console.log("This is the plain Again:",plainAgain);
+    alert("n: "+(n*1)+"\np: "+(p*1)+"\nq: "+(q*1)+"\nphi: "+(1*phi)+"\ne: "+(e*1)+ "\nPublic Key: ("+(e*1)+","+(n*1)+")\nPrivate Key: ("+(d*1)+","+(1*phi)+") \nCipher Text-> "+Cipher+"\nDecrypted Text-> "+plainAgain);
+}
